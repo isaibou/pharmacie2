64e426938b425facd1d69f20896584003602921d
isaibou <62399006+isaibou@users.noreply.github.com>
2021-01-19 22:53:15 +0100

    first commit

---
+++ Pharmacie2/src/main/java/com/example/demo/Pharmacie2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pharmacie2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pharmacie2Application.class, args);
+	}
+
+}
